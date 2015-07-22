9ab075d71d15c684c3f7e8a858b416dd144ad63c
Lana Black <lanablack@amok.cc>
2015-07-22 12:53:54 +0000

    initial

---
+++ Database/Persist/Instances/IP.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings  #-}
+module Database.Persistent.Instances.IP where
+
+import Data.ByteString.Char8 (pack,unpack)
+import Data.IP
+import Data.IP.Internal
+import Data.Maybe (fromMaybe)
+import Database.Persist
+import Database.Persist.Types
+import Database.Persist.Sql
+import Text.Read (readMaybe)
+
+instance PersistField IP where
+    toPersistValue = PersistDbSpecific . pack . show
+
+    fromPersistValue (PersistDbSpecific v) = fromMaybe (Left "Unable to parse IP") (pure <$> readMaybe (unpack v))
+    fromPersistValue _ = Left "IP must be converted from PersistDbSpecific"
+
+instance PersistFieldSql IP where
+    sqlType _ = SqlOther "INET"
+
+instance PersistField IPRange where
+    toPersistValue = PersistDbSpecific . pack . show
+
+    fromPersistValue (PersistDbSpecific v) = fromMaybe (Left "Unable to parse IPRange") (pure <$> readMaybe (unpack v))
+    fromPersistValue _ = Left "IPRange must be converted from PersistDbSpecific"
+
+instance PersistFieldSql IPRange where
+    sqlType _ = SqlOther "CIDR"
