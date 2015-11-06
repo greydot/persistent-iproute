abd1d6c19c9f2741f7f173ed4121ddcbdb19d74a
Lana Black <lanablack@amok.cc>
2015-11-06 14:50:13 +0000

    Move Aeson instances to a separate package (aeson-iproute)

--- Database/Persist/Instances/IP.hs
+++ Database/Persist/Instances/IP.hs
@@ -5,8 +5,7 @@ module Database.Persist.Instances.IP where
 #if __GLASGOW_HASKELL__ <= 708
 import Control.Applicative (pure, (<$>))
 #endif
-import Data.Aeson
-import Data.Aeson.Types (typeMismatch)
+import Data.Aeson.IP ()
 import Data.ByteString.Char8 (pack,unpack)
 import Data.IP (IPRange, IP)
 import Data.Maybe (fromMaybe)
@@ -34,23 +33,9 @@ instance PersistField IPRange where
 instance PersistFieldSql IPRange where
     sqlType _ = SqlOther "CIDR"
 
-instance FromJSON IPRange where
-    parseJSON (String s) = maybe (fail "Failed to parse IPRange") pure (readMaybe $ T.unpack s)
-    parseJSON v = typeMismatch "IPRange" v
-
-instance ToJSON IPRange where
-    toJSON = String . T.pack . show
-
 instance PathPiece IPRange where
     fromPathPiece = readMaybe . T.unpack
     toPathPiece = T.pack . show
-
-instance FromJSON IP where
-    parseJSON (String s) = maybe (fail "Failed to parse IP") pure (readMaybe $ T.unpack s)
-    parseJSON v = typeMismatch "IP" v
-
-instance ToJSON IP where
-    toJSON = String . T.pack . show
 
 instance PathPiece IP where
     fromPathPiece = readMaybe . T.unpack
