a9f4e2a9cb51ae69f3f996a10540a32e15495cce
Maximilian Tagher <feedback.tagher@gmail.com>
2019-11-29 11:25:21 -0500

    Fix Either/FilterValue compiler error
    It looks like in old versions of persistent (e.g. https://hackage.haskell.org/package/persistent-2.9.2/docs/Database-Persist-Types.html), Filter had these constructors:
    
    ```
    data Filter record = forall typ. PersistField typ => Filter
        { filterField  :: EntityField record typ
        , filterValue  :: Either typ [typ] -- FIXME
        , filterFilter :: PersistFilter -- FIXME
        }
        | FilterAnd [Filter record] -- ^ convenient for internal use, not needed for the API
        | FilterOr  [Filter record]
        | BackendFilter
              (BackendSpecificFilter (PersistEntityBackend record) record)
    ```
    
    but in newer versions, it changed (https://hackage.haskell.org/package/persistent-2.10.4/docs/Database-Persist-Types.html#t:Filter):
    
    ```
    data Filter record = forall typ. PersistField typ => Filter
        { filterField  :: EntityField record typ
        , filterValue  :: FilterValue typ
        , filterFilter :: PersistFilter -- FIXME
        }
        | FilterAnd [Filter record] -- ^ convenient for internal use, not needed for the API
        | FilterOr  [Filter record]
        | BackendFilter
              (BackendSpecificFilter (PersistEntityBackend record) record)
    ```
    
    resulting in this compiler error:
    
    ```
    /Users/maximiliantagher/Documents/Clones/Haskell/persistent-iproute/Database/Persist/IP.hs:42:71: error:
        • Couldn't match expected type ‘FilterValue IP’
                      with actual type ‘Either IP b0’
        • In the second argument of ‘Filter’, namely ‘(Left ip)’
          In the expression:
            Filter
              (unsafeCoerce field :: EntityField record IP)
              (Left ip)
              (BackendSpecificFilter ">>")
          In an equation for ‘>.>.’:
              field >.>. ip
                = Filter
                    (unsafeCoerce field :: EntityField record IP)
                    (Left ip)
                    (BackendSpecificFilter ">>")
       |
    42 | field >.>. ip = Filter (unsafeCoerce field :: EntityField record IP) (Left ip) (BackendSpecificFilter ">>")
       |                                                                       ^^^^^^^
    ```
    
    `FilterValue` is defined like this:
    
    ```
    data FilterValue typ where
      FilterValue  :: typ -> FilterValue typ
      FilterValues :: [typ] -> FilterValue typ
      UnsafeValue  :: forall a typ. PersistField a => a -> FilterValue typ
    ```
    
    So, my reading of the situation is that it needed a third constructor, so what was previously `Left`/`Right` is now `FilterValue`/`FilterValues`, and just a switchout is necessary. I compiled with:
    
    ```
    extra-deps: [persistent-2.10.4]
    ```
    
    to confirm this fixes the compilation error.
    
    I haven't tested this at runtime though.

--- Database/Persist/IP.hs
+++ Database/Persist/IP.hs
@@ -19,24 +19,24 @@ import Unsafe.Coerce (unsafeCoerce)
 
 -- | The record range is contained within the specified range. Corresponds to PgSQL operator <<.
 (<:<.) :: EntityField record IPRange -> IPRange -> Filter record
-field <:<. range = Filter field (Left range) (BackendSpecificFilter "<<")
+field <:<. range = Filter field (FilterValue range) (BackendSpecificFilter "<<")
 
 -- | The record range contains the specified range. Corresponds to PgSQL operator >>.
 (>:>.) :: EntityField record IPRange -> IPRange -> Filter record
-field >:>. range = Filter field (Left range) (BackendSpecificFilter ">>")
+field >:>. range = Filter field (FilterValue range) (BackendSpecificFilter ">>")
 
 -- | The record range is contained within or equals to the specified range. Corresponds to PgSQL operator <<=.
 (<:<=) :: EntityField record IPRange -> IPRange -> Filter record
-field <:<= range = Filter field (Left range) (BackendSpecificFilter "<<=")
+field <:<= range = Filter field (FilterValue range) (BackendSpecificFilter "<<=")
 
 -- | The record range contains or equals to the specified range. Corresponds to PgSQL operator >>=.
 (>:>=) :: EntityField record IPRange -> IPRange -> Filter record
-field >:>= range = Filter field (Left range) (BackendSpecificFilter ">>=")
+field >:>= range = Filter field (FilterValue range) (BackendSpecificFilter ">>=")
 
 -- | The record address is contained within the specified range. Corresponds to PgSQL operator <<.
 (<.<.) :: EntityField record IP -> IPRange -> Filter record
-field <.<. range = Filter (unsafeCoerce field :: EntityField record IPRange) (Left range) (BackendSpecificFilter "<<")
+field <.<. range = Filter (unsafeCoerce field :: EntityField record IPRange) (FilterValue range) (BackendSpecificFilter "<<")
 
 -- | The record range contains the specified address. Corresponds to PgSQL operator >>.
 (>.>.) :: EntityField record IPRange -> IP -> Filter record
-field >.>. ip = Filter (unsafeCoerce field :: EntityField record IP) (Left ip) (BackendSpecificFilter ">>")
+field >.>. ip = Filter (unsafeCoerce field :: EntityField record IP) (FilterValue ip) (BackendSpecificFilter ">>")
